c1866fe09f336503f046c916067e041b5898d42c
sachin <sachin.sharma@apnamart.in>
2025-01-06 18:11:35 +0530

    keystore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,23 +27,17 @@ android {
     }
 
     signingConfigs {
-        create("release") {
-            keyAlias = System.getenv("KEY_ALIAS")
-                ?: project.findProperty("android.injected.signing.key.alias") as String?
-            keyPassword = System.getenv("KEY_PASSWORD")
-                ?: project.findProperty("android.injected.signing.key.password") as String?
-            storeFile = System.getenv("KEYSTORE_FILE")?.let { file(it) }
-                ?: project.findProperty("android.injected.signing.store.file")
-                    ?.let { file(it as String) }
-            storePassword = System.getenv("KEYSTORE_PASSWORD")
-                ?: project.findProperty("android.injected.signing.store.password") as String?
-        }
+      create("release"){
+          storeFile = rootProject.file("keystore")
+          keyAlias = "sachin"
+          keyPassword = "sachinsharma"
+          storePassword = "sachinsharma"
+      }
     }
 
     buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true
+        release {
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -95,63 +89,63 @@ dependencies {
 
 
     implementation(libs.androidx.hilt.work)
-    implementation(libs.retrofit)
-    implementation(libs.converter.gson)
-    implementation(libs.androidx.multidex)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.androidx.multidex)
     implementation(libs.play.services.mlkit.barcode.scanning)
     // CameraX core library using the camera2 implementation
-    implementation(libs.androidx.camera.camera2)
+    implementation (libs.androidx.camera.camera2)
     // If you want to additionally use the CameraX Lifecycle library
-    implementation(libs.androidx.camera.lifecycle)
+    implementation( libs.androidx.camera.lifecycle)
     // If you want to additionally use the CameraX View class
-    implementation(libs.androidx.camera.view)
+    implementation (libs.androidx.camera.view)
     // If you want to additionally use the CameraX Extensions library
-    implementation(libs.androidx.camera.extensions)
+    implementation (libs.androidx.camera.extensions)
 
     // Import the BoM for the Firebase platform
-    implementation(platform(libs.firebase.bom))
+    implementation (platform(libs.firebase.bom))
     // Declare the dependencies for the Crashlytics and Analytics libraries
     // When using the BoM, you don"t specify versions in Firebase library dependencies
-    implementation(libs.firebase.crashlytics.ktx)
-    implementation(libs.firebase.analytics.ktx)
-    implementation(libs.firebase.messaging.ktx)
-    implementation(libs.firebase.perf)
-    implementation(libs.firebase.firestore.ktx)
+    implementation (libs.firebase.crashlytics.ktx)
+    implementation (libs.firebase.analytics.ktx)
+    implementation (libs.firebase.messaging.ktx)
+    implementation (libs.firebase.perf)
+    implementation (libs.firebase.firestore.ktx)
 
     // HTTP
-    implementation(libs.retrofit)
-    implementation(libs.converter.gson)
-    implementation(libs.okhttp)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.okhttp)
 
     // Room Persistence
-    implementation(libs.androidx.room.runtime)
-    ksp(libs.androidx.room.compiler)
+    implementation (libs.androidx.room.runtime)
+    ksp (libs.androidx.room.compiler)
     // For Kotlin use kapt instead of annotationProcessor
-    implementation(libs.androidx.room.ktx)
-    testImplementation(libs.androidx.room.testing)
+    implementation (libs.androidx.room.ktx)
+    testImplementation (libs.androidx.room.testing)
     // location
-    implementation(libs.play.services.location)
+    implementation (libs.play.services.location)
 
-    implementation(libs.org.eclipse.paho.client.mqttv3)
+    implementation (libs.org.eclipse.paho.client.mqttv3)
     //paging
-    implementation(libs.androidx.paging.runtime.ktx)
+    implementation (libs.androidx.paging.runtime.ktx)
 
     // LifeCycle Aware Components
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.common.java8)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.androidx.lifecycle.common.java8)
+    implementation (libs.androidx.lifecycle.runtime.ktx)
     // optional - ProcessLifecycleOwner provides a lifecycle for the whole application process
-    implementation(libs.androidx.lifecycle.process)
-    implementation(libs.androidx.work.runtime.ktx)
+    implementation (libs.androidx.lifecycle.process)
+    implementation (libs.androidx.work.runtime.ktx)
 
     implementation(libs.kotlinx.serialization.json)
 
     //Dagger Hilt
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.compiler)
-    implementation(libs.androidx.hilt.navigation.compose)
+    implementation (libs.hilt.android)
+    kapt (libs.hilt.compiler)
+    implementation (libs.androidx.hilt.navigation.compose)
 
-    implementation(libs.otpview.compose)
+    implementation (libs.otpview.compose)
 
 }
